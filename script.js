6ed38c620c5a29acfce50c9b9c01c31da54ea644
archanaanandhan <141989161+archanaanandhan@users.noreply.github.com>
2023-11-02 14:27:02 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -44,177 +44,186 @@ sum+=num[i];
 })(numbers);
  
 
-//  // return all prime number in an array anonymous function
-var arr2=[11,12,13,14,15,16,17];
-const prime_an=function (arr2){
-    for(let i=0;i<arr2.length;i++){
-        if(arr2%i===0)
-            return false;
-        }
-        return arr2!==1;
+ // return all prime number in an array anonymous function
+ let prime_array=function(numArray){
+  numArray = numArray.filter((number) => {
+    for (var i = 2; i <= Math.sqrt(number); i++) {
+      if (number % i === 0){ return false};
     }
-  
-    const primeanonymous=arr2.filter(element=>prime_an(element));
-    console.log(primeanonymous)
+    return true;
+  });
+  console.log(prime_array([1,2,3,4,5,6,7,8]));
+}
   //  return all prime number in array IIEF
-((num)=>{
-    for(let i=2;i<num;i++){
-        if(num%i===0)
-            return false;
-        }
-        num.filter
-        return num!==1;
-       
-})(n);
+  (  
+    function(numArray){
+       numArray = numArray.filter((number) => {
+         for (var i = 2; i <= Math.sqrt(number); i++) {
+           if (number % i === 0) return false;
+         }
+         return true;
+       });
+       console.log(numArray);
+   })([1,2,3,4])
 
  // Convert all the strings to title caps in a string array in anonymous fuction
- const str = "foo bar baz";
-let ans1=function(str) {
-    return str.split(' ').map(function(val){ 
-      return val.charAt(0).toUpperCase() + val.substr(1).toLowerCase();
-    }).join(' ');
+ const str = ["foo bar baz"];
+  let titlecase=function (str){
+    var res=str.toString();
+    var data1=res.toLowerCase().split(" ");
+   for(var i=0;i<data1.length;i++){
+    data1[i]=data1[i].charAt(0).toUpperCase()+data1[i].slice(1);
+
+   }
+    return data1.join(' ')
   }
-let title=ans1(str);
- console.log(title);
+  console.log(titlecase(str));
 
 
  //Convert all the strings to title caps in a string array in iief fuction
-(function strtolitle(str){
-    return str.split(' ').map(function(val){ 
-      return val.charAt(0).toUpperCase() + val.substr(1).toLowerCase();
-    }).join(' ');
-  })(strtolitle(str));
+
+  (function (str){
+    var res=str.toString();
+    var data1=res.toLowerCase().split(" ");
+   for(var i=0;i<data1.length;i++){
+    data1[i]=data1[i].charAt(0).toUpperCase()+data1[i].slice(1);
+
+   }
+    console.log(data1.join(' ')) 
+  })
+  ((str));
   
  // "remove duplicate in array anonymous function")
-let arr = ["apple", "mango", "apple","orange", "mango", "mango"];
-let removeDuplicates=function (arr)  {
-	return arr.filter((item,index) => arr.indexOf(item) === index);
-}
-let removearr=removeDuplicates(arr);
-console.log(removearr);
+let duplicate_arr = ["apple", "mango", 
+          "apple", "orange", "mango", "mango"]; 
+  
+let removeDuplicates=function (arr) { 
+    let a = []; 
+    for (i = 0; i < arr.length; i++) { 
+        if (a.indexOf(arr[i]) === -1) { 
+            a.push(arr[i]); 
+        } 
+    } 
+    return a; 
+} 
+console.log(removeDuplicates(duplicate_arr));
 //romove duplicate from an array iief function
-( (arr)=> {
-	return arr.filter((item,index) => (arr.indexOf(item) === index));
-  })
-  console.log(removeduplicates);
-console.log();
+(function (arr) { 
+  let a = []; 
+  for (i = 0; i < arr.length; i++) { 
+      if (a.indexOf(arr[i]) === -1) { 
+          a.push(arr[i]); 
+      } 
+  } 
+  console.log(a); 
+} )
+(duplicate_arr);
 //Return all the palindromes in an array anonymous function
-const palindromes =function (el)  {
-  const str = String(el);
-  let i = 0;
-  let j = str.length - 1;
-  while(i < j) {
-     if(str[i] === str[j]) {
-        i++;
-        j--;
-     }
-     else {
-        return false;
-     }
+var arr=["mom","dad","data"]
+let palindrome=function (arr){
+var result=[];
+for(i=0;i<arr.length;i++){
+  var data=arr[i].split("").reverse().join("");
+  if(data===arr[i]){
+    result.push (arr[i])
   }
-  return true;
-};
-const palindromeanonymous = function (arr1)  {
-  return arr1.filter(el => palindromes(el));
-};
-console.log(palindromeanonymous(arr1));
+}
+ return result 
+}
+console.log(palindrome(arr));
 //'Return all the palindromes in an array iief function'
-
-( (el1) => {
-  const str = String(el);
-  let i = 0;
-  let j = str.length - 1;
-  while(i < j) {
-     if(str[i] === str[j]) {
-        i++;
-        j--;
-     }
-     else {
-        return false;
-     }
+(function (arr){
+  var result=[];
+  for(i=0;i<arr.length;i++){
+    var data=arr[i].split("").reverse().join("");
+    if(data===arr[i]){
+      result.push (arr[i])
+    }
   }
-  return true;
-});
-const palindromeanonymous1 = function (arr1)  {
-  return arr1.filter(el1 => palindromes(el1));
-};
-console.log((palindromeanonymous(arr1)));
-//Return median of two sorted arrays of the same size.
+  console.log(result)  
+  })
+ ((arr));
+/*//Return median of two sorted arrays of the same size.
 let sort_arr1=[1,2,3,4,5,6,7,8];
 let sort_arr2=[9,10,11,12,13,14];
 let new_arr=sort_arr1.sort(sort_arr2);
-console.log(new_arr);
+console.log(new_arr);*/
 
 
 
 //2. arrow function
 //print all odd number 
-var allOddArr=(arr)=>{
+var allOdd_Arr=(arr)=>{
   for( let i=0;i<arr.length;i++){
-if( arr[i]%2!=0){
+if( arr[i]%2!==0){
 console.log(arr[i]);
 }
 }
 }
-console.log(allOddArr(array));
+allOdd_Arr([1,2,3,4,5,6,7,8]);
  //sum all array element 
-const arrowsum=(num)=>{
+const arrow_sum=(num)=>{
   let sum=0;
 for(i=0;i<num.length;i++)
 {
 sum+=num[i];
-console.log(sum);
+
 } 
+console.log(sum);
 };
-// arrowsum(numbers);
+arrow_sum(numbers);
 // // Return all the prime numbers in an array 
 
-// const n=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+const n=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
 
-// const isPrime = (num) => {
-//   for (let i = 2; i < num; i++) {
-//     if (num % i === 0) return false;
-//   }
-//   return num !== 1;
-// };
-// const myPrimeArray =n.filter(numb=>isPrime(numb));
-// console.log(myPrimeArray);
+const isPrime = (num) => {
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) return false;
+  }
+  return num !== 1;
+};
+const myPrimeArray =n.filter(numb=>isPrime(numb));
+console.log(myPrimeArray);
 
 // //   // convert all the string to title cap in string array in arrow function
-// let ans=(str) =>{
-//     return str.split(' ').map(function(val){ 
-//       return val.charAt(0).toUpperCase() + val.substr(1).toLowerCase();
-//     }).join(' ');
-//   }
-//   let title1=ans(str);
-//   console.log(title)
+const str1 = ["foo bar baz"];
+let titlecase_arrow= (str)=>{
+  var res=str.toString();
+  var data1=res.toLowerCase().split(" ");
+ for(var i=0;i<data1.length;i++){
+  data1[i]=data1[i].charAt(0).toUpperCase()+data1[i].slice(1);
+
+ }
+  return data1.join(' ')
+}
+console.log(titlecase_arrow(str));
   
 // //remove duplicate from an array 
-// let removean=function (arr) {
-// 	return arr.filter((item,index) => (arr.indexOf(item) === index));
-//   }
-// const  removeduplicates=removean(arr)
-// console.log(removeduplicates);
+let duplicate_arr1 = ["apple", "mango", 
+          "apple", "orange", "mango", "mango"]; 
+  
+let removeDuplicates_arrow=(arr)=> { 
+    let a = []; 
+    for (i = 0; i < arr.length; i++) { 
+        if (a.indexOf(arr[i]) === -1) { 
+            a.push(arr[i]); 
+        } 
+    } 
+    return a; 
+} 
+console.log(removeDuplicates_arrow(duplicate_arr));
   
 //  //'Return all the palindromes in an array 
-// const arr1 = ['carecar', 1344, 12321, 'did', 'cannot'];
-// const isPalindrome = el => {
-//    const str = String(el);
-//    let i = 0;
-//    let j = str.length - 1;
-//    while(i < j) {
-//       if(str[i] === str[j]) {
-//          i++;
-//          j--;
-//       }
-//       else {
-//          return false;
-//       }
-//    }
-//    return true;
-// };
-// const findPalindrome = arr1 => {
-//    return arr1.filter(el => isPalindrome(el));
-// };
-// console.log(findPalindrome(arr1));
+var arr=["1221","dad","data"]
+let arrow_palindrome= (arr)=>{
+var result=[];
+for(i=0;i<arr.length;i++){
+  var data=arr[i].split("").reverse().join("");
+  if(data===arr[i]){
+    result.push (arr[i])
+  }
+}
+ return result 
+}
+console.log(arrow_palindrome(arr));
  
